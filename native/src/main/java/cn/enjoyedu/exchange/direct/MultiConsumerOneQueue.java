50270aa85fb9a1a8f780698d82d750bdad27e656
Justin0704 <djlijian@126.com>
2022-05-03 22:02:04 +0800

    rabbitMq - 一个队列多个消费者direct模式

---
+++ native/src/main/java/cn/enjoyedu/exchange/direct/MultiConsumerOneQueue.java
@@ -0,0 +1,13 @@
+package cn.enjoyedu.exchange.direct;
+
+/**
+ * 一个队列多个消费者，则会表现出消息在消费者之间的轮询发送
+ */
+public class MultiConsumerOneQueue {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
