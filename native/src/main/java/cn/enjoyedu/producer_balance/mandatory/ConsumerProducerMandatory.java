f98112634dd7ae2ad2e649ce3ef2da38f7211412
Justin0704 <djlijian@126.com>
2022-05-06 17:38:56 +0800

    生产者发送消息的权衡 - 失败通知（mandatory）

--- native/src/main/java/cn/enjoyedu/producer_balance/mandatory/ConsumerProducerMandatory.java
+++ native/src/main/java/cn/enjoyedu/producer_balance/mandatory/ConsumerProducerMandatory.java
@@ -1,8 +1,44 @@
 package cn.enjoyedu.producer_balance.mandatory;
 
+import cn.enjoyedu.exchange.direct.DirectProducer;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 消费者 - (失败确认模式)
+ */
 public class ConsumerProducerMandatory {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TimeoutException {
 
+//创建连接，连接到rabbitMQ
+        ConnectionFactory factory = new ConnectionFactory();
+        //设置连接工程的默认地址
+        factory.setHost("127.0.0.1");
+        //创建连接
+        Connection connection = factory.newConnection();
+        //创建信道
+        Channel channel = connection.createChannel();
+        //在信道中设置交换器
+        channel.exchangeDeclare(ProducerMandatory.EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        //声明随机队列
+        String queueName = channel.queueDeclare().getQueue();
+        //绑定，将队列（queue-justin）与交换器通过路由键绑定
+        String routeKey = "justin";
+        channel.queueBind(queueName, ProducerMandatory.EXCHANGE_NAME, routeKey);
+        System.out.println("Waiting message...");
+
+        //声明一个消费者
+        final Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("Received: [" + envelope.getRoutingKey() + "]" + message);
+            }
+        };
+        //消费者开始指定队列上的消息
+        channel.basicConsume(queueName, true, consumer);
     }
 }
--- native/src/main/java/cn/enjoyedu/producer_balance/mandatory/ProducerMandatory.java
+++ native/src/main/java/cn/enjoyedu/producer_balance/mandatory/ProducerMandatory.java
@@ -1,9 +1,6 @@
 package cn.enjoyedu.producer_balance.mandatory;
 
-import com.rabbitmq.client.BuiltinExchangeType;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -16,38 +13,48 @@ public class ProducerMandatory {
 
     public static final String EXCHANGE_NAME = "mandatory_test";
 
-    public static void main(String[] args) throws IOException, TimeoutException {
-        //创建连接到rabbitMQ
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        //创建连接，连接到rabbitMQ
         ConnectionFactory factory = new ConnectionFactory();
-        //设置rabbitMQ地址
-        factory.setHost("127.0.0.1");
-        //创建一个连接
+        //设置连接工厂的连接地址（默认端口号为5672）
+        factory.setHost("localhost");
+        //创建连接
         Connection connection = factory.newConnection();
-        //创建一个信道
+        //创建信道
         Channel channel = connection.createChannel();
-        //指定转发
-        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+        //在信道中设置交换器
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
 
-        //TODO 失败通知
-
-
-        //声明路由键
-        String[] teachers = {"justin", "willon", "john"};
-        for(int i = 0; i < 3 ; i ++){
-            String[] modules = {"kafka", "jvm", "redis"};
-            for(int j = 0;j<3;j++){
-                String[] servers = {"A", "B", "C"};
-                for(int k = 0;k<3;k++){
-                    //发送消息
-                    String message = "Hello Topic_["+i+","+j+","+k+"]";
-                    String routeKey = teachers[i%3] + "." + modules[j%3] + "." + servers[k%3];
-                    channel.basicPublish(EXCHANGE_NAME, routeKey, null, message.getBytes());
-                    System.out.println(" [*] Send " + routeKey + " : " + message);
-                }
+        //TODO 失败通知，回调
+        channel.addReturnListener(new ReturnListener() {
+            public void handleReturn(int replyCode, String replyText, String exchange, String routeKey, AMQP.BasicProperties basicProperties, byte[] bytes) throws IOException {
+                String message = new String(bytes, "UTF-8");
+                System.out.println("返回的 replyCode：" + replyCode);
+                System.out.println("返回的 replyText：" + replyText);
+                System.out.println("返回的 exchange：" + exchange);
+                System.out.println("返回的 routeKey：" + routeKey);
             }
+        });
+
+
+        //声明路由键/消息体
+        String[] routeKeys = {"justin", "willon", "john"};
+        for(int i = 0;i<3;i++){
+            String routeKey = routeKeys[i % 3];
+            String msg = "Hello RabbitMQ" + (i+1) + ("_" + System.currentTimeMillis());
+            //发消息,true 表示加上了失败的通知
+            channel.basicPublish(EXCHANGE_NAME, routeKey, true,null, msg.getBytes());
+            System.out.println("---------------------------------------------------");
+            System.out.println("Send Message：" + routeKey + ":" + msg);
+            Thread.sleep(200);
         }
-        //关闭信道和连接
-        channel.close();
-        connection.close();
+        //关闭信道
+        if(channel != null){
+            channel.close();
+        }
+        //关闭连接
+        if(connection != null){
+            connection.close();
+        }
     }
 }
